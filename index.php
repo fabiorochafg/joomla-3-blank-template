a12a9ad157698b2e089909606cd036eee52141e2
Fábio Rocha <fabiorochafg@gmail.com>
2013-03-22 09:46:06 -0300

    Adding CSS and renaming modules.

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <?php
-
 defined('_JEXEC') or die;
 
+$app = JFactory::getApplication();
 $doc = JFactory::getDocument();
+$menu = JSite::getMenu();
+$option = JRequest::GetWord('option');
+
 $doc -> addStyleSheet('templates/' . $this->template . '/css/main.css');
 $doc -> addScript('/templates/' . $this->template . '/js/main.js', 'text/javascript');
-
-$app = JFactory::getApplication();
-$templateparams = $app->getTemplate(true)->params;
+$showLeftColumn = $this->countModules('left');
+$showRightColumn = $this->countModules('right');
+$classContent = 'nobar';
+if ($option != 'com_search') {
+    if ($showLeftColumn && $showRightColumn) {
+        $classContent = 'bothbar';
+    } else if (!$showLeftColumn && $showRightColumn) {
+        $classContent = 'rightbar';
+    } else if ($showLeftColumn && !$showRightColumn) {
+        $classContent = 'leftbar';
+    }
+}
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
@@ -16,33 +28,79 @@ $templateparams = $app->getTemplate(true)->params;
     <jdoc:include type="head" />
 </head>
 
-<body>
-<div class="container">
-    <div id="header">
+<?php if ($menu->getActive() == $menu->getDefault()) { ?>
+    <body class="index">
+<?php } else { ?>
+    <body>
+<?php } ?>
+
+    <div id="header" class="container">
+
         <h1>
-            <a href="<?php echo $this->baseurl; ?>"><?php echo htmlspecialchars($this->params->get('sitedescription')); ?></a>
+            <a href="<?php echo $this->baseurl; ?>"><?php echo $app->getCfg('sitename'); ?></a>
         </h1>
+
+        <?php if($this->countModules('search')) : ?>
+            <div id="search">
+                <jdoc:include type="modules" name="search" style="none" />
+            </div>
+        <?php endif; ?>
+
+        <?php if ($this->countModules('menu')) : ?>
+            <div id="menu">
+                <jdoc:include type="modules" name="menu" style="none" />
+            </div>
+        <?php endif; ?>
+
     </div>
 
-    <div class='mid_container'>
+    <div id="main" class="container">
 
-        <div class='main_content_area'>
-            <jdoc:include type="modules" name="position-3" style="xhtml" />
+        <?php if ($option != 'com_search' && $this->countModules('left')) : ?>
+            <div id="left">
+                <jdoc:include type="modules" name="left" style="xhtml" />
+            </div>
+        <?php endif; ?>
+
+        <div id="content" class="<?php echo $classContent; ?>">
+
+            <?php if($this->countModules('breadcrumbs')) : ?>
+                <div id="breadcrumbs">
+                    Você está em: <jdoc:include type="modules" name="breadcrumbs" style="none" />
+                </div>
+            <?php endif; ?>
+
             <jdoc:include type="message" />
-            <jdoc:include type="component" />
-            <jdoc:include type="modules" name="position-2" style="none" />
+
+            <?php if ($menu->getActive() != $menu->getDefault()) { ?>
+                <jdoc:include type="component" />
+            <?php } ?>
+
+            <?php if ($option != 'com_search' && $this->countModules('content')) : ?>
+                <jdoc:include type="modules" name="content" style="xhtml" />
+            <?php endif; ?>
+
         </div>
 
-        <div class='right_sidebar'>
-            <jdoc:include type="modules" name="position-7" style="well" />
-        </div>
+        <?php if ($option != 'com_search' && $this->countModules('right')) : ?>
+            <div id="right">
+                <jdoc:include type="modules" name="right" style="xhtml" />
+            </div>
+        <?php endif; ?>
 
     </div>
 
-    <div class='footer'>
-        Footer
+    <div id="footer" class="container">
+
+        <?php if($this->countModules('footer')) : ?>
+            <jdoc:include type="modules" name="footer" style="none" />
+        <?php endif; ?>
+
+        <?php echo '&copy;' . date('Y') . ' A blank Template for Joomla 3.0'; ?>
+
     </div>
-    
-</div>
+
+    <jdoc:include type="modules" name="debug" style="none" />
+
 </body>
 </html>
---
+++ js/main.js
@@ -0,0 +1,5 @@
+/* JS Document - Developed by Fabio Rocha (https://github.com/fabiorochafg | fabiorochafg@gmail.com) */
+
+$(document).ready(function() {
+
+});
